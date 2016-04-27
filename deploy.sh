454dbba88aa4c6ee701c8e52f1923f4cc8a07d5a
Scisco <scisco7@gmail.com>
2016-04-27 16:18:04 -0400

    simplify deploy

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 set -e # halt script on error
 
 zip -r updater.zip . --include=*.py
-iron worker upload --max-concurrency $2 --zip updater.zip --name $1 astrodigital/sentinel2-metadata:latest python iron.py
+iron worker upload --max-concurrency 1 --zip updater.zip --name updater astrodigital/sentinel2-metadata:latest python iron.py
